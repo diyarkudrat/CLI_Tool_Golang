81e288628e9a96d8872d39271ba724baedc5f286
Diyar Kudrat <dkudrat@gmail.com>
2020-11-17 23:27:49 -0800

    created struct and validated flag arguments in cli

--- csv2json.go
+++ csv2json.go
@@ -1,9 +1,15 @@
 package main
 
+import (
+	"errors"
+	"flag"
+	"os"
+)
+
 type inputFile struct {
 	filepath  string
 	separator string
-	pretty    bool
+	valid     bool
 }
 
 func main() {
@@ -11,5 +17,27 @@ func main() {
 }
 
 func getFileData() (inputFile, error) {
+
+	// Make sure we receive correct amount of arguments
+	if len(os.Args) < 2 {
+		return inputFile{}, errors.New("filepath argument is required")
+	}
+
+	// flag declarations for CLI
+	separator := flag.String("separator", "comma", "Column separator")
+	valid := flag.Bool("valid", false, "Generate valid JSON data")
+
+	flag.Parse()
+
+	// Store file path value from given argument in the CLI
+	filePath := flag.Arg(0)
+
+	// Validate that the argument for the separator flag was either "comma" or "semicolon"
+	if !(*separator == "comma" || *separator == "semicolon") {
+		return inputFile{}, errors.New("only comma and semicolon separators are allowed")
+	}
+
+	// return struct with its required validated data
+	return inputFile{filePath, *separator, *valid}, nil
 
 }
